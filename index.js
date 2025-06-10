e79f935bc3601012f6f8a474021aafb63ed048ae
pantasuman6 <pantasuman6@gmail.com>
2025-06-10 07:45:29 -0400

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function add(a, b) {
-  return a + b;
+function add(a, b, c) {
+  return a + b +c;
 }
 
-console.log(add(3, 5)); // Output: 8
+console.log(add(3, 7, 1)); 
+
+
+
+function squareRoot(num) {
+  if (num < 0) {
+    return "Invalid input.";
+  }
+  return Math.sqrt(num);
+}
+
+console.log(squareRoot(9));
